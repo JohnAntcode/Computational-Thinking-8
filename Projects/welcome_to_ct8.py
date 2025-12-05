4add1622a630c0b931317891f1a5f539cbb3eee8
JohnAntcode <johnanthonynaiden@seattleacademy.org>
2025-12-05 21:48:49 +0000

    test

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("Im Puerto Rican")                                  
+"I just had lunch with Hobbes"
+"I had a cat"
