524c9d7ffa455a0d9c243f043afe6b240046a309
JohnAntcode <johnanthonynaiden@seattleacademy.org>
2026-01-22 17:27:09 +0000

    actually done now

--- Projects/project2.py
+++ Projects/project2.py
@@ -4,46 +4,46 @@ Chill_points = 0
 
 input("please answer all questions in capital letters!")
 answer1 = input("Do you prefer A teamwork, B points, or C food?   ")
-if answer1 == "A":
+if answer1 == "A" or answer1 == "a":
     Basketball_points += 1
-elif answer1 == "B":
+elif answer1 == "B" or answer1 == "b":
     Soccer_points += 1
-elif answer1 == "C":
+elif answer1 == "C" or answer1 == "c":
     Chill_points += 1
 
 answer2 = input("Is your favorite Season A Summer, B Winter, or C Fall?   ")
-if answer2 == "A":
+if answer2 == "A" or answer2 == "a":
     Soccer_points += 1
     Basketball_points += 1
-elif answer2 == "B":
+elif answer2 == "B" or answer2 == "b":
     Chill_points += 2
-elif answer2 == "C":
+elif answer2 == "C" or answer2 == "c" :
     Basketball_points += 2
 
 answer3 = input("Would you rather A go and play after practice, B don't go to practice, or C go and play hard during practice?")
-if answer3 == "A":
+if answer3 == "A" or answer3 == "a":
     Basketball_points += 4
-elif answer3 == "B":
+elif answer3 == "B" or answer3 == "b":
     Chill_points += 3
-elif answer3 == "C":
+elif answer3 == "C" or answer3 == "c":
      Soccer_points += 3
      Basketball_points += 3
 
 answer4 = input("Would you rather A go to the Fahbbes party, B Not go to the Fahbbes party, or C Start a rebellion agianst the Fahbbes party?")
-if answer4 == "A":
+if answer4 == "A" or answer4 == "a":
     Basketball_points += 5
-elif answer4 == "B":
+elif answer4 == "B" or answer4 == "b":
     Soccer_points += 4
-elif answer4 == "C":
+elif answer4 == "C" or answer4 == "c":
     Soccer_points += 4
     Chill_points += 4
 
-answer5 = input("Would you rather A get cooked Henry in a 1v1, B cook Henry in a 1v1, or C dont even play the 1v1?")
-if answer5 == "A":
+answer5 = input("Would you rather A get by cooked Henry in a 1v1, B cook Henry in a 1v1, or C dont even play the 1v1?")
+if answer5 == "A" or answer5 == "a":
     Soccer_points += 4.5
-elif answer5 == "B":
+elif answer5 == "B" or answer5 == "b":
     Basketball_points += 6
-elif answer5 == "C":
+elif answer5 == "C" or answer5 == "c":
     Chill_points += 5
 
 print(f"Your score is {Basketball_points} Basketball, {Soccer_points} Soccer, and {Chill_points} No sport yet")
